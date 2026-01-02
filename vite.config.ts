2ebf125ebf070e1ab7ca427432fb71f7718a8fff
louis-desenv <128602112+louis-desenv@users.noreply.github.com>
2026-01-02 15:35:12 -0300

    add base prop to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,26 +3,27 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-        allowedHosts: [
-          'darlena-plankless-ernest.ngrok-free.dev',
-          '.ngrok-free.app',
-          '.ngrok.io'
-        ]
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
+  const env = loadEnv(mode, '.', '');
+  return {
+    base: '/sake-e-learning/', // 👈 replace with your GitHub repo name
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
+      allowedHosts: [
+        'darlena-plankless-ernest.ngrok-free.dev',
+        '.ngrok-free.app',
+        '.ngrok.io'
+      ]
+    },
+    plugins: [react()],
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
       }
-    };
+    }
+  };
 });
